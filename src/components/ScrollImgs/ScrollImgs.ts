897e77a254aabeb313486aed6b95ee5dec724687
libin <1963482624@qq.com>
2019-12-11 19:22:20 +0800

    update ScrollImgs.vue

--- src/components/ScrollImgs/ScrollImgs.ts
+++ src/components/ScrollImgs/ScrollImgs.ts
@@ -4,4 +4,6 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 export default class ScrollImgs extends Vue {
     // the scroll imgs, inclding some text, img's url and the destination's url
     @Prop({ default: [{}] }) private imgs!: object[];
+    @Prop({ default: Array(1000) }) private init!: object[];
+
 }
